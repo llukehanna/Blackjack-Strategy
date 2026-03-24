a0c43c0c060f7e32981658ba04dbf76bb6c7295d
Luke Hanna <lllukehanna@gmail.com>
2026-03-24 16:52:04 -0700

    feat(02.2-01): add CornerRadius design token enum
    - Named constants: card, button, statCard (8pt), banner (10pt)
    - Caseless enum for non-instantiable namespace

---
+++ BJS/Design/CornerRadius.swift
@@ -0,0 +1,8 @@
+import CoreGraphics
+
+enum CornerRadius {
+    static let card: CGFloat = 8
+    static let button: CGFloat = 8
+    static let statCard: CGFloat = 8
+    static let banner: CGFloat = 10
+}
